c670a1e23f69f1cd47b8c245def47d5169925313
MegaDr <megadwiranirodja@gmail.com>
2020-05-13 20:30:59 +0700

    Interface Printer

---
+++ interface/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interface2723
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrinterWindows printer;
+            Console.WriteLine("Pilih Printer :");
+            Console.WriteLine("1. Epson");
+            Console.WriteLine("2. Canon");
+            Console.WriteLine("3. LaserJet");
+
+            Console.WriteLine("Pilih Nomer Printer [1/2/3] : ");
+
+            int NomorPrinter = Convert.ToInt32(Console.ReadLine());
+            
+
+            if (NomorPrinter == 1)
+            {
+                printer = new Epson();
+            }
+            else if (NomorPrinter == 2)
+            {
+                printer = new Canon();
+            }
+            else
+            {
+                printer = new LaserJet();
+            }
+
+            printer.Show();
+            printer.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
